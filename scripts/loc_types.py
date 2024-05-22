72f84b23475d9d7f8ff8192917ebc2d14c13bb12
Felix Wiegand <fxwiegand@gmail.com>
2024-05-22 11:05:46 +0200

    Rename columns

--- scripts/loc_types.py
+++ scripts/loc_types.py
@@ -2,15 +2,18 @@ import pandas as pd
 import re
 from collections import defaultdict
 
-# Read input and output file paths from Snakemake
 input_scripts = snakemake.input.scripts
 output_csv = snakemake.output.summary
 
-# Initialize a list to hold DataFrames
 dataframes = []
 
-# Regular expression to match the line type comments
 line_type_pattern = re.compile(r'#\s*(\w+)')
+
+tag_mapping = {
+    'pd': 'plot definition',
+    'i': 'imports',
+    'io': 'io'
+}
 
 for script_path in input_scripts:
     line_counts = defaultdict(int)
@@ -20,18 +23,15 @@ for script_path in input_scripts:
             match = line_type_pattern.search(line)
             if match:
                 line_type = match.group(1)
+                line_type = tag_mapping.get(line_type, line_type)
                 line_counts[line_type] += 1
 
     df = pd.DataFrame(list(line_counts.items()), columns=['LineType', 'Count'])
-
     library_name = script_path.split('/')[-2]
     df['Library'] = library_name
-
     dataframes.append(df)
 
 consolidated_df = pd.concat(dataframes, ignore_index=True)
-
 pivot_df = consolidated_df.pivot_table(index='Library', columns='LineType', values='Count', fill_value=0)
 pivot_df = pivot_df.astype(int)
-
 pivot_df.to_csv(output_csv)
